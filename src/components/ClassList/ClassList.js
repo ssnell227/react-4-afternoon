a20483311bce5e6ec60c8758797c094a80363ec5
ssnell227 <ssnell227@gmail.com>
2020-05-28 14:09:44 -0600

    finished basics

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
-    
+  constructor(props) {
+    super(props)
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(res => this.setState({ students: res.data }))
+      .catch(err => console.log(err))
   }
 
   render() {
+    console.log(this.state.students)
+    const studentsMap = this.state.students.map((student, index) => <Link
+      to={`/student/${student.id}`} 
+      key={`student-${index}`}
+      ><h3 >{`${student.first_name} ${student.last_name}`}</h3></Link>)
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {studentsMap}
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class Student extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.state = {
+      studentInfo: {}
+    }
+  }
 
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(res => this.setState({ studentInfo: res.data }))
+      .catch(err => console.log(err))
   }
 
   render() {
+    const { studentInfo } = this.state
+    console.log(studentInfo)
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{studentInfo.first_name} {studentInfo.last_name}</h1>
+        <h3>Grade: {studentInfo.grade}</h3>
+        <h3>Email: {studentInfo.email}</h3>
       </div>
     )
   }
